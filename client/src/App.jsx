31a52d1499855d4218d3c22df8138ac03f9c6593
Ozkan Sengul <ozzkansengul@gmail.com>
2024-04-03 16:10:47 +0300

    React-router Dom added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,22 @@
 import "./layout.scss";
+import HomePage from "./routes/homePage/homePage";
+import Layout from "./routes/layout/layout";
+import ListPage from "./routes/list/listPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 function App() {
-  return <div className="layout">Hello World</div>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { path: "/", element: <HomePage /> },
+        { path: "/list", element: <ListPage /> },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
